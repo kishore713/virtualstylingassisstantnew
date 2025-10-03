004cde445a17601db2dc6a1bade2f2c47b5160e1
kishore713 <110197757+kishore713@users.noreply.github.com>
2025-10-03 12:25:33 +0530

    Add files via upload

---
+++ styling-app-suite-full/rn-styling-app/App.js
@@ -0,0 +1,1 @@
+import React from "react";import { View, Text } from "react-native";export default function App(){return (<View><Text>RN Styling App MVP</Text></View>);}
---
+++ styling-app-suite-full/web-styling-app/src/App.js
@@ -0,0 +1,2 @@
+import React, { useRef, useState } from "react";
+export default function App(){return <div>Styling Web App MVP</div>}
